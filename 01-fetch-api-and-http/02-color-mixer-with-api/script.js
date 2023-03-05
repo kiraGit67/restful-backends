486176fa5c833c6dd56b3ea5b9c543345158a62b
kiraGit67 <kirsten@sais.de>
2023-03-05 16:40:13 +0100

    feat: adds localStorage function to script.js

--- 01-fetch-api-and-http/02-color-mixer-with-api/script.js
+++ 01-fetch-api-and-http/02-color-mixer-with-api/script.js
@@ -2,7 +2,33 @@
 
 // https://dummy-apis.netlify.app/api/color
 
+function localStorageColorData(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
 const randomColorButton = document.querySelector("#random-color");
+
+const hexValue = document.querySelector("#hex-value");
+const inputRed = document.querySelector("#red");
+const inputGreen = document.querySelector("#green");
+const inputBlue = document.querySelector("#blue");
+
+if (localStorage.getItem("color") !== "") {
+  console.log(localStorage.getItem("color"));
+  document.body.style.backgroundColor = JSON.parse(
+    localStorage.getItem("color")
+  );
+  randomColorButton.style.backgroundColor = JSON.parse(
+    localStorage.getItem("color")
+  );
+  hexValue.innerHTML = JSON.parse(localStorage.getItem("color"));
+}
+
+if (localStorage.getItem("rgb") !== {}) {
+  inputRed.value = JSON.parse(localStorage.getItem("rgb")).r;
+  inputGreen.value = JSON.parse(localStorage.getItem("rgb")).g;
+  inputBlue.value = JSON.parse(localStorage.getItem("rgb")).b;
+}
 
 function colorMixer() {
   fetch("https://dummy-apis.netlify.app/api/color")
@@ -11,26 +37,19 @@ function colorMixer() {
       return response.json();
     })
     .then((jsonData) => {
-      console.log("jsonColorData", jsonData.color);
-      console.log("jsonRGBdata", jsonData.rgb);
-      console.log("jsonColorDataRed", jsonData.rgb.r);
-      console.log("jsonColorDataGreen", jsonData.rgb.g);
-      console.log("jsonColorDataBlue", jsonData.rgb.b);
-
       document.body.style.backgroundColor = jsonData.color;
       randomColorButton.style.backgroundColor = jsonData.color;
 
-      const hexValue = document.querySelector("#hex-value");
       hexValue.innerText = jsonData.color;
-
-      const inputRed = document.querySelector("#red");
       inputRed.value = jsonData.rgb.r;
-
-      const inputGreen = document.querySelector("#green");
       inputGreen.value = jsonData.rgb.g;
-
-      const inputBlue = document.querySelector("#blue");
       inputBlue.value = jsonData.rgb.b;
+
+      localStorageColorData("color", jsonData.color);
+      localStorageColorData("rgb", jsonData.rgb);
+
+      //localStorage.setItem("color", JSON.stringify(jsonData.color));
+      //localStorage.setItem("rgb", JSON.stringify(jsonData.rgb));
     });
 }
 
